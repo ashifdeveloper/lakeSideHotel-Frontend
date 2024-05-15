7c64cf53723b085ad3be74e0daaa6461a69b9ac7
Mohammad Ashif <mohdashif995@gmail.com>
2024-05-15 12:10:09 +0530

    part1

---
+++ src/components/admin/Admin.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Admin = () => {
+  return (
+    <section className='container mt-5'>
+      <h2>Welcome Admin Panel</h2>
+      <hr/>
+      <Link to={"/add-room"}>
+      Manage Rooms
+      
+      
+      </Link>
+      
+
+    </section>
+  )
+}
+
+export default Admin
